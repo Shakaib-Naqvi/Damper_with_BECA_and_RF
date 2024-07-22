6ccf26cc2ee0be3a88b062082ed5ed6cf6860b86
Shakaib Naqvi <55704107+Shakaib-Naqvi@users.noreply.github.com>
2024-07-22 15:24:07 +0500

    Delete RF_Switch.h

--- RF_Switch.h
+++
@@ -1,7 +0,0 @@
-// Define the GPIO pin connected to the RF switch control input
-const int RF_Switch_Pin = 26;  // GPIO 2 on ESP32
-
-bool RF_Remote(){
-  bool status = digitalRead(RF_Switch_Pin);
-  return status;
-}
