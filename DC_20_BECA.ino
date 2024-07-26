11acec7a4e6596f5e7f0e19a39d42c44b0fc8a1a
Shakaib Naqvi <55704107+Shakaib-Naqvi@users.noreply.github.com>
2024-07-26 15:08:02 +0500

    Add files via upload

--- DC_20_BECA.ino
+++ DC_20_BECA.ino
@@ -1,4 +1,4 @@
-#define DEBUG
+// #define DEBUG
 
 #define RF_DATA_PIN 27
 
@@ -7,7 +7,7 @@ const int LED_Pin = 22;
 
 int servo_open_pos = 135;
 int servo_close_pos = 25;
-int last_pos_servo = servo_close_pos;
+int last_pos_servo;
 int servo_delay = 35;
 bool beca_status = false;
 volatile bool rfDataReceived = false;  // Flag to indicate RF data received
@@ -26,8 +26,9 @@ int cfmbutton = 0;
 int lst_cfmbutton = 0;
 int bp = 0;
 bool cfm_flag = false;
+int save_setpointt = 0;
 int CFM_s;
-
+int minval, maxval;
 #include "Servo.h"
 // #include "ServerForWiFiCredentials.h"
 #include "RES_MODBUS_V1_3.h"
@@ -36,6 +37,36 @@ int CFM_s;
 
 Preferences preferences;
 
+void Pot_Calib(int min, int max) {
+  if (min == 0 || max == 0) {
+    if (min == 0) {
+      myservo.write(servo_close_pos);
+      delay(1000);
+      int potmin = analogRead(34);
+      delay(1000);
+      preferences.begin("Pot", false);
+      preferences.putInt("Min", potmin);
+      preferences.end();
+#ifdef DEBUG
+      Serial.print("PotMin:");
+      Serial.println(potmin);
+#endif
+    }
+    if (max == 0) {
+      myservo.write(servo_open_pos);
+      delay(1000);
+      int potmax = analogRead(34);
+      delay(1000);
+      preferences.begin("Pot", false);
+      preferences.putInt("Max", potmax);
+      preferences.end();
+#ifdef DEBUG
+      Serial.print("potmax:");
+      Serial.println(potmax);
+#endif
+    }
+  }
+}
 
 
 void Beep(int beepDelay, int numberOfBeeps) {
@@ -48,8 +79,10 @@ void Beep(int beepDelay, int numberOfBeeps) {
 }
 
 int ReadPot(int potPin) {
-  int minValue = 1300;  // at servo_close_pos
-  int maxValue = 1610;  // at servo_open_pos raw value
+
+  int minValue = minval;  // at servo_close_pos
+  int maxValue = maxval;
+
 
   float potValue = analogRead(potPin);
   int mappedValue = map(potValue, minValue, maxValue, 0, 100);
@@ -59,16 +92,38 @@ int ReadPot(int potPin) {
   } else if (mappedValue > maxValue) {
     maxValue = mappedValue;
   }
+#ifdef DEBUG
+  Serial.println("Pot Value");
+  Serial.println(potValue);
+  Serial.println("Mapped Value+");
+  Serial.println(mappedValue);
+#endif
   return mappedValue;
 }
 
 void setup() {
+  Int_Servo();
+  // preferences.begin("Pot", false);
+  // preferences.putInt("Min", 0);
+  // preferences.putInt("Max", 0);
+  // preferences.end();
+
 #ifdef DEBUG
   Serial.begin(115200);
 #endif
+
+  preferences.begin("Pot", false);
+  minval = preferences.getInt("Min", 0);
+  maxval = preferences.getInt("Max", 0);
+  preferences.end();
+
+  Pot_Calib(minval, maxval);
+
+  int last_pot_value = ReadPot(34);
+  last_pos_servo = map(last_pot_value, 0, 100, servo_close_pos, servo_open_pos);
   pinMode(Buzzer_Pin, OUTPUT);
   digitalWrite(Buzzer_Pin, LOW);
-  Int_Servo();
+
   MoveServo(servo_open_pos, 1, servo_delay);
   delay(1000);
   MoveServo(servo_close_pos, 1, servo_delay);
@@ -101,13 +156,15 @@ void loop() {
     }
     if (bp == 3 && cfm_flag == false) {
       cfm_flag = true;
+      save_setpointt = setpointt * 10;
       Beep(50, 3);
     } else if (bp == 3 && cfm_flag == true) {
       cfm_flag = false;
+      writeSingleRegister(1, 0x03, save_setpointt);
       Beep(1000, 1);
     }
   }
-  if (cfm_flag == true) {
+  if (cfm_flag == true && beca_power == true) {
 
     if (setpointt > 19 && setpointt < 31) {
       int cfm = (setpointt - 20) * 10;
@@ -115,9 +172,28 @@ void loop() {
       preferences.putInt("cfm", cfm);
       preferences.end();
       CFM_s = map(cfm, 0, 100, servo_close_pos, servo_open_pos);
-
+      MoveServo(CFM_s, 1, servo_delay);
       // Serial.println(cfm);
       // Serial.println(CFM_s);
+    }
+  }  //
+  else {
+    /*---------------------- Cool Mode ---------------------*/
+    if (beca_mode == 0 || beca_mode == 2) {
+      if (last_setpoint > temp_by_beca && beca_power == true) {
+        MoveServo(servo_close_pos, 1, servo_delay);
+      } else if (last_setpoint <= temp_by_beca && beca_power == true) {
+        MoveServo(CFM_s, 1, servo_delay);
+      }
+    }
+
+    /*---------------------- Heat Mode ---------------------*/
+    else if (beca_mode == 1) {
+      if (last_setpoint <= temp_by_beca && beca_power == true) {
+        MoveServo(servo_close_pos, 1, servo_delay);
+      } else if (last_setpoint > temp_by_beca && beca_power == true) {
+        MoveServo(CFM_s, 1, servo_delay);
+      }
     }
   }
   // rf_signal = digitalRead(RF_DATA_PIN);
@@ -142,24 +218,6 @@ void loop() {
       previousMillis = currentMillis;
     } else if (currentMillis - previousMillis >= interval) {
       show_on_led(0, temp_by_beca);
-    }
-  }
-
-  /*---------------------- Cool Mode ---------------------*/
-  if (beca_mode == 0 || beca_mode == 2) {
-    if (last_setpoint > temp_by_beca && beca_power == true) {
-      MoveServo(servo_close_pos, 1, servo_delay);
-    } else if (last_setpoint <= temp_by_beca && beca_power == true) {
-      MoveServo(CFM_s, 1, servo_delay);
-    }
-  }
-
-  /*---------------------- Heat Mode ---------------------*/
-  else if (beca_mode == 1) {
-    if (last_setpoint <= temp_by_beca && beca_power == true) {
-      MoveServo(servo_close_pos, 1, servo_delay);
-    } else if (last_setpoint > temp_by_beca && beca_power == true) {
-      MoveServo(CFM_s, 1, servo_delay);
     }
   }
 }
